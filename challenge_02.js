08db3a0b7f36b0987d11fc17cdc7372be1f5c24e
Thirhag <meigraf@hotmail.de>
2023-01-17 07:47:51 +0100

    simpel

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,25 @@ Alter 13-17 // Cola
 -->zB: "Max trinkt Cola."
 
 /* Getränke-Challenge */
+let namen, alter;
+
+Name = prompt("Bitte einen Namen eingeben");
+Age = prompt("Bitte ein Alter eingeben"); 
+
+
+if (Age > 0 && Age < 6)
+{
+console.log(namen + " " + "trinkt Milch");
+}
+else if (Age >= 6 && Age <= 12) 
+{
+console.log(Name + " " + "trinkt Saft");
+} 
+else if (Age >= 13 && Age <=17 ) 
+{
+console.log(Name + " " + "trinkt Cola");
+} 
+else
+{
+console.log(Name + " " + "trinkt Wein");
+} 
