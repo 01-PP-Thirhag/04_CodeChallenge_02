46f6373ac3a4c780272de3ea3b2daf7c2e7d5c85
Thirhag <meigraf@hotmail.de>
2023-01-17 09:14:18 +0100

    Var Case

--- challenge_02.js
+++ challenge_02.js
@@ -15,25 +15,72 @@ Alter 13-17 // Cola
 -->zB: "Max trinkt Cola."
 
 /* Getränke-Challenge */
-let Name, Age;
+// let Name, Age;
 
-Name = prompt("Bitte einen Namen eingeben");
-Age = prompt("Bitte ein Alter eingeben"); 
+// Name = prompt("Bitte einen Namen eingeben");
+// Age = prompt("Bitte ein Alter eingeben"); 
 
 
-if (Age > 0 && Age < 6)
+// if (Age > 0 && Age < 6)
+// {
+// console.log(Name + " " + "trinkt Milch");
+// }
+// else if (Age >= 6 && Age <= 12) 
+// {
+// console.log(Name + " " + "trinkt Saft");
+// } 
+// else if (Age >= 13 && Age <=17 ) 
+// {
+// console.log(Name + " " + "trinkt Cola");
+// } 
+// else
+// {
+// console.log(Name + " " + "trinkt Wein");
+// } 
+
+/*******Variante Case ************/
+let alter, nutzer;
+
+
+
+alter = prompt("Bitte Alter angeben:");
+
+nutzer = prompt("Bitte Namen angeben:");
+
+
+
+switch (true)
+
 {
-console.log(Name + " " + "trinkt Milch");
+
+        case (alter <=5):
+
+            console.log(nutzer +" trinkt Milch");
+
+            break;
+
+        case (alter >= 6 && alter <=12):
+
+            console.log(nutzer +" trinkt Saft");
+
+            break;
+
+        case (alter >=13 && alter <18):
+
+            console.log(nutzer +" trinkt Cola");
+
+            break;
+
+        case (alter >=18):
+
+            console.log(nutzer +" trinkt Wein");
+
+            break;
+
+        default:
+
+        console.log("Die Eingabe ist nicht korrekt!")
+
+            break;
+
 }
-else if (Age >= 6 && Age <= 12) 
-{
-console.log(Name + " " + "trinkt Saft");
-} 
-else if (Age >= 13 && Age <=17 ) 
-{
-console.log(Name + " " + "trinkt Cola");
-} 
-else
-{
-console.log(Name + " " + "trinkt Wein");
-} 
